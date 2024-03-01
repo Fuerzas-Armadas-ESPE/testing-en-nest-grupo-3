2f6e2657b3f24e2d3671e3d9448b5a600acea252
karol-macas <klmacas@espe.edu.ec>
2024-02-29 23:44:00 -0500

    testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://root:example@localhost:27017'),
+    MongooseModule.forRoot('mongodb://root:example@localhost:27017', { dbName: 'mydatabase' }),
     PostsModule,
   ],
 })
